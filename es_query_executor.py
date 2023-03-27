11372a59e39217a593016b54f5b783141328cf1c
Rishi Verma <riverma@apache.org>
2023-03-27 15:14:00 -0700

    Fixed script bugs and added delete / cleanup scenarios per Nick

--- es_query_executor.py
+++ es_query_executor.py
@@ -8,21 +8,21 @@ from elasticsearch import Elasticsearch
 
 # Set up configuration
 logging_level = logging.INFO
-log_folder = sys.argv[0]
+log_folder = "es_query_executor"
 
 # Set up command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--host", help="Elasticsearch host and port", required=True)
 parser.add_argument("--index", help="Elasticsearch index name", required=True)
 parser.add_argument("--query_file", help="Path to the JSON file containing the Elasticsearch query", required=True)
+parser.add_argument("--action", help="Action to invoke for query, i.e. 'search' or 'delete'", required=True)
 args = parser.parse_args()
 
 # Set up logging to a rolling file, within a new logs sub-folder
 logging.basicConfig(
-    filename=f"{log_folder}/{datetime.now().strftime('%Y%m%d_%H%M%S')}.log",
+    filename=f"{log_folder}/{datetime.now().strftime('%Y%m%d')}.log",
     level=logging_level,
-    format="%(asctime)s %(message)s",
-    handlers=[logging.FileHandler(log_folder+"/rolling.log", mode="a")]
+    format="%(asctime)s %(message)s"
 )
 
 # Connect to Elasticsearch
@@ -32,12 +32,25 @@ es = Elasticsearch([args.host])
 with open(args.query_file, "r") as f:
     query = json.load(f)
 
-# Execute the query on Elasticsearch with specified index
-res = es.search(index=args.index, body=query)
+# Execute the query on Elasticsearch with specified index per action requested
+if (args.action == "search"):
+    res = es.search(index=args.index, body=query)
 
-# Log the detailed results
-logging.info(res)
+    # Log the detailed results
+    logging.info(res)
 
-# Print the document IDs that were affected by the query
-doc_ids = [hit["_id"] for hit in res["hits"]["hits"]]
-print("Affected document IDs:", doc_ids)
+    print("Affected documents:", res['hits']['total']['value'])
+elif (args.action == "delete"):
+    res = es.delete_by_query(index=args.index, body=query)
+
+    # Log the detailed results
+    logging.info(res)
+
+    print("Affected documents:", res['deleted'])
+else:
+    print("Invalid --action value. Choose from 'search' or 'delete'")
+    sys.exit(1)
+
+
+
+
