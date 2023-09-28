bd2c36dce57430e44a2bde8793916f1c67aafc2f
philipjyoon <104090313+philipjyoon@users.noreply.github.com>
2023-09-28 08:50:26 -0700

    Update trigger_from_list.sh for SLC downloads

--- trigger_from_list.sh
+++ trigger_from_list.sh
@@ -33,12 +33,14 @@ if [[ $2 == "--dryrun" || $2 == "-d" ]]; then
 	DRYRUN=true
 fi
 
-RELEASE_VERSION="2.0.0-rc.3.0"
+RELEASE_VERSION="2.0.0"
 
 for granule in $( cat $LIST ); do 
 
 	## HLS PROCESSING
 	if [[ ${granule:0:3} == "HLS" ]] ; then
+
+		job_queue="opera-job_worker-hls_data_download" 
 
 		if  [[ ${granule:4:1} == "S" ]]; then
 			collection="HLSS30"
@@ -51,9 +53,13 @@ for granule in $( cat $LIST ); do
 	## SLC PROCESSING
 	elif [[ ${granule:0:3} == "S1A" ]]; then
 		collection="SENTINEL-1A_SLC"
+		job_queue="opera-job_worker-slc_data_download"
+		granule+="*"
 	
 	elif [[ ${granule:0:3} == "S1B" ]]; then
                 collection="SENTINEL-1B_SLC"
+		job_queue="opera-job_worker-slc_data_download"
+		granule+="*"
 
 	else
 		bad_granule $granule
@@ -61,9 +67,9 @@ for granule in $( cat $LIST ); do
 
 
 	if $DRYRUN; then
-		echo "~/mozart/ops/opera-pcm/data_subscriber/daac_data_subscriber.py query -c $collection --release-version=$RELEASE_VERSION --job-queue=opera-job_worker-hls_data_download --chunk-size=1 --native-id=$granule"
+		echo "~/mozart/ops/opera-pcm/data_subscriber/daac_data_subscriber.py query -c $collection --release-version=$RELEASE_VERSION --job-queue=$job_queue --chunk-size=1 --native-id=$granule"
 	else
-		~/mozart/ops/opera-pcm/data_subscriber/daac_data_subscriber.py query -c $collection  --release-version=$RELEASE_VERSION --job-queue=opera-job_worker-hls_data_download --chunk-size=1 --native-id=$granule
+		~/mozart/ops/opera-pcm/data_subscriber/daac_data_subscriber.py query -c $collection  --release-version=$RELEASE_VERSION --job-queue=$job_queue --chunk-size=1 --native-id=$granule
 	fi
 
 done
