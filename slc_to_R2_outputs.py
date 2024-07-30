223cd30a1d367799525dc8d04f63afcef6f2d71e
niarenaw <niarenaw@gmail.com>
2024-07-30 18:49:44 -0400

    parameterize R2 output

--- slc_to_R2_outputs.py
+++ slc_to_R2_outputs.py
@@ -41,26 +41,62 @@ def get_body(slc) -> dict:
     }
 
 
+def check_slc(slc) -> dict:
+    return {
+        "query": {
+            "bool": {
+                "must": [{"match": {
+                    "metadata.id.keyword": slc
+                }}],
+                "must_not": [],
+                "should": []
+            }
+        },
+        "from": 0,
+        "size": 500,
+        "sort": [],
+        "aggs": {}
+    }
+
+
+RTC_INDEX = 'grq_v1.0_l2_rtc_s1-*'
+CSLC_INDEX = 'grq_v1.0_l2_cslc_s1-2023.10'
+SLC_INDEX = 'grq_1_l1_s1_slc-2024.04'
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='get output products from list of SLC granules')
     parser.add_argument('file')
+    parser.add_argument('product_type', choices=['rtc', 'cslc', 'slc'])
+    parser.add_argument('--missing', default=False, action='store_true')
     args = parser.parse_args()
 
+    if args.product_type == 'cslc':
+        INDEX = CSLC_INDEX
+        LOOKUP = get_body
+    elif args.product_type == 'rtc':
+        INDEX = RTC_INDEX
+        LOOKUP = get_body
+    else:
+        INDEX = SLC_INDEX
+        LOOKUP = check_slc
+
     with open(args.file, 'r') as f:
+        
         for granule in f.readlines():
-            granule = granule.strip('\n')
-            body = get_body(granule)
-            print(f'>>> {granule}: ')
-            # cslc_res = es.search(index='grq_v0.0_l2_cslc_s1*', body=body, size=500)
-            rtc_res = es.search(index='grq_v0.4_l2_rtc_s1*', body=body, size=500)
-            # for hit in cslc_res['hits']['hits']:
-            #     print(hit['_id'])
-            # for hit in rtc_res['hits']['hits']:
-            #     print(hit['_id'])
-            print(len(rtc_res['hits']['hits']))
-            # if len(rtc_res['hits']['hits']) == 0:
-                # print(granule)
-            # print()
+            # remove -SLC if it exists (not in _id in GRQ)
+            granule = granule.replace('-SLC', '').strip('\n')
+
+            res = es.search(index=INDEX, body=LOOKUP(granule), size=500)
+            
+            if args.missing:
+                num_hits = len(res['hits']['hits'])
+                if num_hits == 0:
+                    print(granule)
+            else:
+                for hit in (res['hits']['hits']):
+                    print(hit['_id'])
+            
 
 
